ba60ab011ba301c4cb7c3aca91d421dde6b36631
chengbin_liu <chengbin_liu@hotmail.com>
2019-05-19 09:44:55 +0800

    init log

--- log/log.go
+++ log/log.go
@@ -1,83 +1,196 @@
 package log
 
 import (
+	"bytes"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
+	"log"
+	"os"
+	"path"
+	"sync"
+	"time"
 )
 
-var (
-	// the local logger
-	logger *zap.SugaredLogger
-)
+var instance *zap.Logger
+var once sync.Once
 
-func init() {
-	fileName := "hzlb-srv.log"
-	syncWriter := zapcore.AddSync(&lumberjack.Logger{
-		Filename:  fileName,
-		MaxSize:   1, //MB
-		LocalTime: true,
-		Compress:  true,
+func Logger(serviceName string) *zap.Logger {
+	once.Do(func() {
+		instance = NewLogger(serviceName)
 	})
-	encoder := zap.NewProductionEncoderConfig()
-	encoder.EncodeTime = zapcore.ISO8601TimeEncoder
-	core := zapcore.NewCore(zapcore.NewJSONEncoder(encoder), syncWriter, zap.NewAtomicLevelAt(zap.DebugLevel))
-	log := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
-	logger = log.Sugar()
+	return instance
 }
 
-func Debug(args ...interface{}) {
-	logger.Debug(args...)
+// Init 初始化
+func Init(serviceName string) *zap.Logger {
+	instance = NewLogger(serviceName)
+	return instance
 }
 
-func Debugf(template string, args ...interface{}) {
-	logger.Debugf(template, args...)
+// NewLogger 新建日志
+func NewLogger(srvName string) *zap.Logger {
+
+	directory := path.Join("./log", srvName)
+	writers := []zapcore.WriteSyncer{newRollingFile(directory)}
+	writers = append(writers, os.Stdout)
+	logger, dyn := newZapLogger(true, zapcore.NewMultiWriteSyncer(writers...))
+	zap.RedirectStdLog(logger)
+
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		for range ticker.C {
+			updateLogLevel(srvName, dyn, false)
+		}
+	}()
+
+	return logger
 }
 
-func Info(args ...interface{}) {
-	logger.Info(args...)
+func updateLogLevel(serviceName string, dyn *zap.AtomicLevel, isProduction bool) {
+
+	originLevelString := "info"
+	if !isProduction {
+		originLevelString = "debug"
+	}
+
+	levelConf := make(map[string]map[string]string)
+
+	newLevelString, ok := levelConf[serviceName]["127.0.0.1"]
+	if !ok {
+		newLevelString, ok = levelConf[serviceName]["*"]
+		if !ok {
+			newLevelString = originLevelString
+		}
+	}
+
+	if !ok {
+		newLevelString, ok = levelConf[serviceName]["*"]
+		if !ok {
+			newLevelString = originLevelString
+		}
+	}
+
+	newLevel := new(zapcore.Level)
+	if err := newLevel.Set(newLevelString); err != nil {
+		newLevel.Set(originLevelString)
+	}
+	if dyn.Level() != *newLevel {
+		log.Println("修改日志等级: ", dyn.Level().String(), "=>", newLevel.String())
+		dyn.SetLevel(*newLevel)
+	}
 }
 
-func Infof(template string, args ...interface{}) {
-	logger.Infof(template, args...)
+func newRollingFile(directory string) zapcore.WriteSyncer {
+	if err := os.MkdirAll(directory, 0766); err != nil {
+		log.Println("failed create log directory:", directory, ":", err)
+		return nil
+	}
+
+	return newLumberjackWriteSyncer(&lumberjack.Logger{
+		Filename:  path.Join(directory, "output.log"),
+		MaxSize:   100, //megabytes
+		MaxAge:    7,   //days
+		LocalTime: true,
+		Compress:  false,
+	})
 }
 
-func Warn(args ...interface{}) {
-	logger.Warn(args...)
+func newZapLogger(isProduction bool, output zapcore.WriteSyncer) (*zap.Logger, *zap.AtomicLevel) {
+	encCfg := zapcore.EncoderConfig{
+		TimeKey:        "@timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+		EncodeDuration: zapcore.NanosDurationEncoder,
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+		},
+	}
+
+	var encoder zapcore.Encoder
+	dyn := zap.NewAtomicLevel()
+	if isProduction {
+		dyn.SetLevel(zap.InfoLevel)
+		encCfg.EncodeLevel = zapcore.LowercaseLevelEncoder
+		encoder = zapcore.NewConsoleEncoder(encCfg) // zapcore.NewJSONEncoder(encCfg)
+	} else {
+		dyn.SetLevel(zap.DebugLevel)
+		encCfg.EncodeLevel = zapcore.LowercaseColorLevelEncoder
+		encoder = zapcore.NewConsoleEncoder(encCfg)
+	}
+
+	return zap.New(zapcore.NewCore(encoder, output, dyn), zap.AddCaller()), &dyn
 }
 
-func Warnf(template string, args ...interface{}) {
-	logger.Warnf(template, args...)
+type lumberjackWriteSyncer struct {
+	*lumberjack.Logger
+	buf       *bytes.Buffer
+	logChan   chan []byte
+	closeChan chan interface{}
+	maxSize   int
 }
 
-func Error(args ...interface{}) {
-	logger.Error(args...)
+func newLumberjackWriteSyncer(l *lumberjack.Logger) *lumberjackWriteSyncer {
+	ws := &lumberjackWriteSyncer{
+		Logger:    l,
+		buf:       bytes.NewBuffer([]byte{}),
+		logChan:   make(chan []byte, 5000),
+		closeChan: make(chan interface{}),
+		maxSize:   1024,
+	}
+	go ws.run()
+	return ws
 }
 
-func Errorf(template string, args ...interface{}) {
-	logger.Errorf(template, args...)
+func (l *lumberjackWriteSyncer) run() {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			if l.buf.Len() > 0 {
+				l.sync()
+			}
+		case bs := <-l.logChan:
+			_, err := l.buf.Write(bs)
+			if err != nil {
+				continue
+			}
+			if l.buf.Len() > l.maxSize {
+				l.sync()
+			}
+		case <-l.closeChan:
+			l.sync()
+			return
+		}
+	}
 }
 
-func DPanic(args ...interface{}) {
-	logger.DPanic(args...)
+func (l *lumberjackWriteSyncer) Stop() {
+	close(l.closeChan)
 }
 
-func DPanicf(template string, args ...interface{}) {
-	logger.DPanicf(template, args...)
+func (l *lumberjackWriteSyncer) Write(bs []byte) (int, error) {
+	b := make([]byte, len(bs))
+	for i, c := range bs {
+		b[i] = c
+	}
+	l.logChan <- b
+	return 0, nil
 }
 
-func Panic(args ...interface{}) {
-	logger.Panic(args...)
+func (l *lumberjackWriteSyncer) Sync() error {
+	return nil
 }
 
-func Panicf(template string, args ...interface{}) {
-	logger.Panicf(template, args...)
-}
-
-func Fatal(args ...interface{}) {
-	logger.Fatal(args...)
-}
-
-func Fatalf(template string, args ...interface{}) {
-	logger.Fatalf(template, args...)
+func (l *lumberjackWriteSyncer) sync() error {
+	defer l.buf.Reset()
+	_, err := l.Logger.Write(l.buf.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
 }
